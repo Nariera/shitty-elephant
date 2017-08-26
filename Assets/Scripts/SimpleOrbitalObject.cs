3c043410c25e39c0b8200c6dd038d850dd96b702
Alec Asperslag <aaspersl@ucsc.edu>
2017-08-26 01:14:44 -0700

    Alecs really simple orbital object
    And scene to go with it.

---
+++ Assets/Scripts/SimpleOrbitalObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleOrbitalObject : MonoBehaviour {
+
+    public Transform GravityWell;
+    public float GravAcceleration = 2;
+
+    public Transform selfTransform;
+    Rigidbody2D self;
+
+	// Use this for initialization
+	void Start () {
+        selfTransform = gameObject.transform;
+        self = gameObject.GetComponent<Rigidbody2D>();
+
+        //Just want to add some initial propulsion
+        self.AddForce(selfTransform.up*100);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Gravity.  Add Force of Accelaration * direction
+        Vector2 dir = selfTransform.position - GravityWell.position;
+        self.AddForce(dir.normalized * -GravAcceleration);
+
+
+    }
+}
