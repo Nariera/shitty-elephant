f29ba40bda6a7447bd86d5e15762a691a2ef6125
Alec Asperslag <aaspersl@ucsc.edu>
2017-08-26 12:42:05 -0700

    Added Planet template
    That thing i just said.  Added some generic image assests for planets,
    made a prefab with colliders for atmosphere and impact.  Impact Death
    will reset the scene.

---
+++ Assets/Scripts/HitDeathCollider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HitDeathCollider : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //We probably want this to check for general orbital objects if those will exist and can burn up.
+        PlayerControl thePlayer = other.GetComponent<PlayerControl>();
+        if(thePlayer != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
