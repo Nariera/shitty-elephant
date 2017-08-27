ee6470737b0d1708d9c3662356e31cabbd2a9385
Alec Asperslag <aaspersl@ucsc.edu>
2017-08-26 23:44:50 -0700

    Added story points to main scene
    DONT WORRY ABOUT ALL THE LASERS

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Alec is tired, this script is definitely a mistake.
+/// </summary>
+[RequireComponent(typeof(LineRenderer))]
+public class Laser : MonoBehaviour {
+
+    LineRenderer LaserLine;
+    public float forceOfLaser = 1;
+
+    public GameObject orbitalTarget = null;
+    public Rigidbody2D selfRB;
+
+	// Use this for initialization
+	void Start () {
+        LaserLine = GetComponent<LineRenderer>();
+        LaserLine.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (orbitalTarget == null)
+        {
+            LaserLine.enabled = false;
+            return;
+        }
+
+        LaserLine.enabled = true;
+        LaserLine.SetPosition(0, transform.position);
+        LaserLine.SetPosition(1, orbitalTarget.transform.position);
+
+        Vector2 dir = transform.position - orbitalTarget.transform.position;
+        Rigidbody2D targetRB = orbitalTarget.GetComponent<Rigidbody2D>();
+        if (targetRB != null)
+            targetRB.AddForce(dir.normalized * forceOfLaser);
+        selfRB.AddForce(-dir.normalized * forceOfLaser);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Orbital")
+        {
+            orbitalTarget = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Orbital")
+        {
+            orbitalTarget = null;
+        }
+    }
+}
