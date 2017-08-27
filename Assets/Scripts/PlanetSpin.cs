6520d4b85287f3019475e9c6b85da13365613dfa
Tsai <angelofangle@gmail.com>
2017-08-27 01:08:18 -0700

    Hopefull no breaks

---
+++ Assets/Scripts/PlanetSpin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSpin : MonoBehaviour
+{
+	public float spedAngle;
+	private float realSpedAngle = 0;
+
+	void Update()
+	{
+		transform.Rotate(0, 0, realSpedAngle);
+	}
+
+	void Start()
+	{
+		realSpedAngle = Random.Range(spedAngle / 2, spedAngle);
+		if (Random.Range(0, 2) == 0)
+			realSpedAngle = -realSpedAngle;
+	}
+}
